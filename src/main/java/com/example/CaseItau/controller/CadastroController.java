2d5ff208084c063b173ebcaf67956fb49c76aa98
Debora <debora.carvalho589@gmail.com>
2023-08-04 13:38:34 -0300

    Criação métodos PATCH e DELETE

--- src/main/java/com/example/CaseItau/controller/CadastroController.java
+++ src/main/java/com/example/CaseItau/controller/CadastroController.java
@@ -1,11 +1,16 @@
 package com.example.CaseItau.controller;
 
 import com.example.CaseItau.model.Cadastro;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.CaseItau.service.CadastroService;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -25,7 +30,23 @@ public class CadastroController {
     }
 
     @PostMapping("/cadastro")
-    public Cadastro criarCadastro(@RequestBody Cadastro cadastro) {
+    public Cadastro criarCadastro(@RequestBody Cadastro cadastro)
+    {
         return cadastroService.criarCadastro(cadastro);
     }
+
+    @PatchMapping("/cadastro/{id}")
+    public Cadastro atualizarParcial(@PathVariable("id") Long id, @RequestBody Map<Object, Object> data){
+        return cadastroService.atualizarParcial(id, data);
+    }
+
+    @DeleteMapping("/cadastro/{id}")
+    public ResponseEntity<String> deletarCadastro(@PathVariable Long id) {
+        if(cadastroService.deletarCadastro(id)) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Conta excluída com sucesso!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Conta não encontrada!");
+        }
+    }
+
 }
--- src/main/java/com/example/CaseItau/service/CadastroService.java
+++ src/main/java/com/example/CaseItau/service/CadastroService.java
@@ -2,11 +2,25 @@ package com.example.CaseItau.service;
 
 import com.example.CaseItau.repository.CadastroRepository;
 import com.example.CaseItau.model.Cadastro;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -15,7 +29,8 @@ public class CadastroService {
     @Autowired
     private CadastroRepository cadastroRepository;
 
-    public Optional<Cadastro> listarCadastroId(Long id){
+
+    public Optional<Cadastro> listarCadastroId(Long id) {
         return cadastroRepository.findById(id);
     }
 
@@ -26,6 +41,17 @@ public class CadastroService {
     public Cadastro criarCadastro(Cadastro cadastro) {
         return cadastroRepository.save(cadastro);
     }
+
+    public Cadastro atualizarParcial(Long id, Map<Object, Object> objectMap) {
+        Cadastro cadastro = cadastroRepository.findById(id).get();
+        objectMap.forEach((key, value) -> {
+            Field field = ReflectionUtils.findField(Cadastro.class, (String) key);
+            field.setAccessible(true);
+            ReflectionUtils.setField(field, cadastro, value);
+        });
+        return cadastroRepository.save(cadastro);
+    }
+
 
     @Transactional
     public boolean deletarCadastro(Long id) {
